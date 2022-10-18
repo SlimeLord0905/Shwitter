a92e7f152a7c1c21e4ab72b654641e52300c5443
Mathieu Mercier <merciermathieu412@gmail.com>
2022-10-18 09:11:54 -0400

    Login/signup started

---
+++ app/src/main/java/com/mathieu_mercier/shwitter/logingInActivity.java
@@ -0,0 +1,14 @@
+package com.mathieu_mercier.shwitter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class logingInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loging_in);
+    }
+}
